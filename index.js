1dd3a496913382b1e576de5d24abdfd793d557d4
Samudra Kumar Rana <125203284+samudrarana@users.noreply.github.com>
2023-04-09 00:53:54 +0530

    Added roll dice game website files

---
+++ index.js
@@ -0,0 +1,47 @@
+let player1 = "Player 1";
+let player2 = "Player 2";
+function editNames(){
+    player1 = prompt("Enter the Player1 Name:");
+    player2 = prompt("Enter the player2 Name:");
+
+    if(player1.length<1 || player2.length<1){
+        alert("Please enter valid name for both the player!");
+        return;
+    }
+    document.querySelector("p.player1").innerHTML=player1;
+    document.querySelector("p.player2").innerHTML=player2;
+}
+
+
+function rollDice(){
+    var image1 = document.querySelectorAll("img")[0];
+    var image2 = document.querySelectorAll("img")[1];
+
+    image1.setAttribute("src","image/dice.gif");
+    image2.setAttribute("src","image/dice.gif");
+    setTimeout(() =>{
+        var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+        var randomDiceImage1="image/dice"+ randomNumber1 +".png";
+
+    
+        image1.setAttribute("src", randomDiceImage1);
+
+        var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+        var randomDiceImage2="image/dice"+ randomNumber2 +".png";
+
+        
+        image2.setAttribute("src", randomDiceImage2);
+
+        if(randomNumber1 > randomNumber2){
+            document.querySelector("h3").innerHTML = player1+ " Wins!🎊";
+        }
+        else if (randomNumber1 < randomNumber2){
+            document.querySelector("h3").innerHTML=player2+" Wins!🎊";
+        }
+        else{
+            document.querySelector("h3").innerHTML="Draw";
+        }
+    },3000);
+}
